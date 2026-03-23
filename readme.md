3f26df30fda938dc08313144d68fd9546fecc050
Sankalp Sharma <sankalpsharma.896@gmail.com>
2026-03-23 06:40:33 +0530

    Initial commit - Django Todo App

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Django Todo App
+
+A simple Todo app built using Django.
+
+## Features
+- Add tasks
+- Edit tasks
+- Delete tasks
+- Clear all tasks
+
+## Tech Stack
+- Django
+- HTML, CSS
+
+## How to run
+```bash
+python manage.py runserver
---
+++ todo/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Todo
+
+def todo_list(request):
+    todos = Todo.objects.all()
+
+    if request.method == "POST":
+
+        if "add_task" in request.POST:
+            title = request.POST.get("title")
+            if title:
+                Todo.objects.create(title=title)
+            return redirect("todo_list")
+        
+
+        if "edit_task" in request.POST:
+            task_id = request.POST.get("task_id")
+            new_title = request.POST.get("new_title")
+            todo = get_object_or_404(Todo,id=task_id)
+            todo.title = new_title
+            todo.save()
+            return redirect("todo_list")
+
+        if "delete_task" in request.POST:
+            task_id = request.post.get("task_id")
+            todo = get_object_or_404(Todo,id=task_id)
+            todo.delete()
+            return redirect("todo_list")
+        
+        if "clear_all" in request.POST:
+            Todo.objects.all().delete()
+            return redirect("todo_list")
+        
+    return render(request, "todo/list.html", {
+            "todos": todos,
+            "task_count": todos.count()
+        })
+
