742369dbe548609cae45494d4f6b6a702babf208
Sankalp Sharma <sankalpsharma.896@gmail.com>
2026-03-28 20:09:24 +0530

    added description,priority and datetime

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 class Todo(models.Model):
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
     title = models.CharField( max_length=200)
+    description = models.TextField(blank = True)
+    due_date = models.DateTimeField()
+    priority = models.CharField(
+    max_length=1,
+    choices=PRIORITY_CHOICES,
+    default='M',
+    )
+    completed = models.BooleanField(default=False)
+
 
 
 def __str__(self):
--- todo/views.py
+++ todo/views.py
@@ -1,38 +1,50 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 
 def todo_list(request):
-    todos = Todo.objects.all()
+    todos = Todo.objects.all().order_by('-id')
 
     if request.method == "POST":
 
         if "add_task" in request.POST:
             title = request.POST.get("title")
+            description = request.POST.get("description")
+            due_date = request.POST.get("due_date")
+            priority = request.POST.get("priority")
+
             if title:
-                Todo.objects.create(title=title)
+                Todo.objects.create(
+                    title=title,
+                    description=description,
+                    due_date=due_date if due_date else None,
+                    priority=priority if priority else 'M'
+                )
             return redirect("todo_list")
-        
 
         if "edit_task" in request.POST:
             task_id = request.POST.get("task_id")
-            new_title = request.POST.get("new_title")
-            todo = get_object_or_404(Todo,id=task_id)
-            todo.title = new_title
+            todo = get_object_or_404(Todo, id=task_id)
+
+            todo.title = request.POST.get("title")
+            todo.description = request.POST.get("description")
+            todo.due_date = request.POST.get("due_date") or None
+            todo.priority = request.POST.get("priority")
+            todo.completed = True if request.POST.get("completed") == "on" else False
+
             todo.save()
             return redirect("todo_list")
 
         if "delete_task" in request.POST:
             task_id = request.POST.get("task_id")
-            todo = get_object_or_404(Todo,id=task_id)
+            todo = get_object_or_404(Todo, id=task_id)
             todo.delete()
             return redirect("todo_list")
-        
+
         if "clear_all" in request.POST:
             Todo.objects.all().delete()
             return redirect("todo_list")
-        
-    return render(request, "todo/list.html", {
-            "todos": todos,
-            "task_count": todos.count()
-        })
 
+    return render(request, "todo/list.html", {
+        "todos": todos,
+        "task_count": todos.count()
+    })
