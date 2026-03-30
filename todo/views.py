1fd0a1abde277f74d9f19b3abb4f2bcb869e2e8f
Sankalp Sharma <sankalpsharma.896@gmail.com>
2026-03-30 22:58:29 +0530

    fixed datetime bug in edit block

--- todo/views.py
+++ todo/views.py
@@ -32,16 +32,17 @@ def todo_list(request):
         if "edit_task" in request.POST:
             task_id = request.POST.get("task_id")
             todo = get_object_or_404(Todo, id=task_id)
-
             todo.title = request.POST.get("title")
             todo.description = request.POST.get("description")
-            todo.due_date = request.POST.get("due_date") or None
+            due_date = request.POST.get("due_date")
 
             if due_date:
                 due_date = parse_datetime(due_date)
             else:
-                due_date =timezone.now()
-                
+                due_date = timezone.now()  
+
+            todo.due_date = due_date
+
             todo.priority = request.POST.get("priority")
             todo.completed = True if request.POST.get("completed") == "on" else False
 
