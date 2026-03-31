1b5f05424c6fc0944f847a61773170118eb43d80
Sankalp Sharma <sankalpsharma.896@gmail.com>
2026-03-31 08:58:15 +0530

    integrated postgresSQL

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -79,14 +79,7 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': '5432',
-    }
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
